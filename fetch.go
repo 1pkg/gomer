ab423f9653db849d388a2c088e199d8bf32a1b67
Kostiantyn Masliuk <1pkg@protonmail.com>
2021-03-07 11:17:55 +0100

    implement gomod index fetch

--- fetch.go
+++ fetch.go
@@ -2,26 +2,76 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"time"
 )
 
-func fmonth(ctx context.Context, m, y int, mchan chan<- modv) error {
-	from, to := time.Date(y, time.January, 1, 0, 0, 0, 0, time.UTC), time.Date(y, time.January, 30, 0, 0, 0, 0, time.UTC)
+func fmonth(ctx context.Context, y, m int, mchan chan<- modv) error {
+	from := time.Date(y, time.Month(m), 1, 0, 0, 0, 0, time.UTC)
+	to := time.Date(y, time.Month(m), ndays(y, m), 0, 0, 0, 0, time.UTC)
 	for {
-		mods, t, err := fpage(ctx, from)
+		mods, err := fpage(ctx, from)
 		if err != nil {
 			return err
 		}
-		for _, mod := range mods {
-			mchan <- mod
-		}
-		if t.After(to) {
+		if len(mods) == 0 {
 			return nil
 		}
-		from = t
+		for _, mod := range mods {
+			if mod.Timestamp.After(to) {
+				return nil
+			}
+			mchan <- mod
+		}
+		from = mods[len(mods)-1].Timestamp
 	}
 }
 
-func fpage(ctx context.Context, t time.Time) ([]modv, time.Time, error) {
-	return nil, time.Now(), nil
+func fpage(ctx context.Context, t time.Time) ([]modv, error) {
+	url := fmt.Sprintf("https://index.golang.org/index/since=%s", t.String())
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	m := make([]modv, 0, 2000)
+	if err := json.Unmarshal(b, &m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func ndays(y, m int) int {
+	leap := y%4 == 0 && (y%100 != 0 || y%400 == 0)
+	d := days[m-1]
+	if leap && m == 2 {
+		d++
+	}
+	return d
+}
+
+var days = [...]int{
+	31,
+	28,
+	31,
+	30,
+	31,
+	30,
+	31,
+	31,
+	30,
+	31,
+	30,
+	31,
 }
--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"context"
+	"log"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	fmonth(context.TODO(), 1, 2020, nil)
+	g, ctx := errgroup.WithContext(context.Background())
+	ch := make(chan modv, 8000)
+	cy := time.Now().Year()
+	for y := 2019; y <= cy; y++ {
+		for m := 1; m <= 12; m++ {
+			g.Go(func() error {
+				return fmonth(ctx, y, m, ch)
+			})
+		}
+	}
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 type modv struct {
